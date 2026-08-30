fdc07b044bd659742ae9dc03a482a3bcbca1080f
Nkj-718 <jhanitin16@gmail.com>
2026-08-31 01:19:24 +0530

    Learnt Inputs and Outputs of C++

---
+++ Day1/InputOutput.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string name;
+    cin >> name;
+    cout << "Hey " << name << "\n"; //Newline
+    cout << "Hey " << name << endl; //Newline and Flush output buffer
+    return 0;
+}
