dbe942d9fb124a711b3a85363a0c211b873559ce
Nkj-718 <jhanitin16@gmail.com>
2026-08-31 01:21:21 +0530

    Learnt if-else and else-if statements

---
+++ Day1/If_ElseIf.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5;
+    if(a==5){
+        cout<<"YES"<<endl;
+    }
+    if(a==2){
+        cout<<"Hey"<<endl;
+    } else if(a==4){
+        cout<<"Hello"<<endl;
+    } else{
+        cout<<"Hola"<<endl;
+    }
+    return 0;
+}
