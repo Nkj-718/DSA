587597ae6f16d0a6d2974cb50d5329c7c04bbcb2
Nkj-718 <jhanitin16@gmail.com>
2026-09-02 06:20:55 +0530

    Reverse Right angled triangle made up of asterisks.

---
+++ Day 3/Pattern5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Pattern{
+    int size;
+    public:
+        Pattern(int size) : size(size) {}
+        void printPattern(){
+            for(int i=size; i>0; i--){
+                for(int j=1; j<=i; j++){
+                    cout << "*";
+                }
+                cout << "\n";
+            }
+        }
+};
+
+int main(){
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+    Pattern p(size);
+    p.printPattern();
+    return 0;
+}
