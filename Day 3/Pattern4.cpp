4b4ac756fac4819aecdd0d87f6101535fdf4b5ea
Nkj-718 <jhanitin16@gmail.com>
2026-09-02 06:20:03 +0530

    Right angled triangle made up of row number

---
+++ Day 3/Pattern4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Pattern{
+    int size;
+
+public:
+    Pattern(int size) : size(size) {}
+    void printPattern(){
+        for (int i = 1; i <= size; i++){
+            for(int j=1; j<=i; j++){
+                cout << i;
+            }
+            cout << "\n";
+        }
+    }
+};
+
+int main(){
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+    Pattern p(size);
+    p.printPattern();
+    return 0;
+}
