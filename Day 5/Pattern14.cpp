b8f83643997ce9c6d9a2bdb544c45b1701c6564d
Nkj-718 <jhanitin16@gmail.com>
2026-09-04 18:26:29 +0530

    Right angled triangle made up of alphabets.

---
+++ Day 5/Pattern14.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Pattern{
+    int rows;
+    public:
+        Pattern (int rows) : rows(rows) { printPattern(); }
+        void printPattern(){
+            for(int row=0; row<rows; row++){
+                for(char col='A'; col<='A'+row; col++){
+                    cout << col << " ";
+                }
+                cout << "\n";
+            }
+        }
+
+};
+
+int main(){
+    int rows;
+    cout << "Rows: ";
+    cin >> rows;
+    Pattern p(rows);
+}
